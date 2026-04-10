df33cad53297e20a5da1beb243cfa02e88ddbb04
Camila <lchavesac.01@gmail.com>
2026-04-09 20:51:09 -0500

    Agregados los campos faltantes del form

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+    $serverName = "tallermecanicodata.database.windows.net"; // o IP del servidor
+    $connectionOptions = [
+        "Database" => "Taller_Tecnomecanica",
+        "Uid" => "admin_taller4",
+        "PWD" => "Hola12345"
+    ];
+
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+    if ($conn === false) {
+        die(print(sqlsrv_errors(), true));
+    }
+?>
--- ordenTrabajo.php
+++ ordenTrabajo.php
@@ -7,7 +7,9 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/b42da86e0b.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://unpkg.com/dselect@latest/dist/css/dselect.css">
+    <script src="https://unpkg.com/dselect@latest/dist/js/dselect.js"></script>
     </head>
 
 <body>
@@ -30,36 +32,57 @@
     <form class="col-4 offset-4 p-3">
         <h3 class="text-center text-secondary">Registrar Orden de Trabajo</h3>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Nombre del propietario</label>
-            <input type="text" class="form-control" name="nombre">
+            <label for="exampleInputEmail1" class="form-label">Numero de Documento</label>
+            <input type="text" class="form-control" name="numDocumento">
         </div>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Tipo de vehiculo</label>
-            <input type="text" class="form-control" name="vehiculo">
+            <label for="exampleInputEmail1" class="form-label">Vehiculo</label>
+            <select class="form-select" id="vehiculo_input">
+                <option selected>Choose...</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>  
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Servicio</label>
-            <input type="text" class="form-control" name="servicio">
+            <select class="form-select" id="servicio_input">
+                <option selected>Choose...</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>        
         </div>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Repuesto</label>
+            <label for="check">¿El servicio requiere de repuesto?</label>
             <input type="checkbox" id="check" name="repuesto_hay" value="yes">
-            <label for="check">Usa repuesto?</label>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">id repuesto</label>
-                <input type="text" class="form-control" name="id_repuesto">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Trabajador responsable</label>
-                <input type="text" class="form-control" name="nombre">
-            </div>
-             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Valor total</label>
-                <input type="text" class="form-control" name="valor">
-            </div>
-         </div>
-
-            <button type="submit" class="btn btn-primary"name="btnregistrar" value="ok">Registrar</button>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nombre del repuesto</label>
+            <select name="select_box" class="form-select" id="select_box">
+                <option value=""> Selecciona... </option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>   
+            </select>     
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Trabajador responsable</label>
+            <input type="text" class="form-control" name="nombre">
+        </div>
+         <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Valor total</label>
+            <input type="text" class="form-control" name="valor">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Fecha y hora de inicio</label>
+            <input type="datetime-local" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Fecha y hora de finalizacion</label>
+            <input type="datetime-local" class="form-control">
+        </div>
+        <button type="submit" id="btnRegistrarOrdenTrabajo" class="btn btn-primary"name="btnregistrar" value="ok">Registrar</button>
     </form>
     <div class="col-12 p-4">
 <table class="table">
@@ -99,8 +122,10 @@
        </td>
     </tr>
   </tbody>
-</table></div> 
+</table>
+</div> 
 
 </body>
 
 </html>
+
